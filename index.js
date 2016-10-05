a7dbb6ecbcaa9cdb63c00cc6917bfbe05c5c086c
Claudio Yuri <claudio.yuri@gmail.com>
2016-10-05 18:35:23 -0300

    Agrego frontend beta

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /*jshint esversion: 6 */
-var mailer = require("./modules/mailer.js");
-var mongodb = require('mongodb');
-
 var errorMsg = "";
+
 if(!process.env.MONGOCONNECTIONSTRING){
   errorMsg += "Enviroment var MONGOCONNECTIONSTRING not defined\n";
 }
@@ -17,46 +15,69 @@ if(errorMsg !== ""){
   process.exit(1);
 }
 
-var mongoClient = mongodb.MongoClient.connect(process.env.MONGOCONNECTIONSTRING, (err, db) => {
-  if (err) throw err;
-  console.log("Conectado a la base de conocimientos...");
+const express = require('express');
+const listenPort = 8080;
+var app = express();
+var path = __dirname + '/views/';
 
-  console.log("Buscando información...");
+app.get("/", (req, res) => {
+  res.sendFile(path + "index.html");
+});
 
-  //busco los últimos, ordenado por fecha descendente y me quedo con los últimos 3
-  db.collection('ultimos').find({}).sort({fecha: -1}).limit(3).toArray((err, ultimos) => {
-    var ultIds = ultimos.map((x) => x.idLugar);
-    var ultimoTipoDeComida = typeof ultimos[0] == 'undefined' || ultimos[0].tipoDeComida == 'undefined'? [] : ultimos[0].tipoDeComida;
+app.get("/recomendation", (req, res) => {
+  getRecommendation();
+  res.status(200).send("listo");
+});
 
-    //busco todos los lugares disponibles sin contar los últimos seleccionados
-    db.collection('lugares').find({_id: {$nin: ultIds}, tipoDeComida: {$nin: ultimoTipoDeComida} , esLujo: false}).toArray((err, opcionesDeComida) => {
-      if (err) console.log(err);
-      var result = opcionesDeComida;
+app.listen(listenPort, () => {
+  console.log("listening on port " + listenPort);
+});
 
-      console.log("Aplicando algoritmo de desición...");
+function getRecommendation(){
+  var mailer = require("./modules/mailer.js");
+  var mongodb = require('mongodb');
 
-      var length = result.length;
+  var mongoClient = mongodb.MongoClient.connect(process.env.MONGOCONNECTIONSTRING, (err, db) => {
+    if (err) throw err;
+    console.log("Conectado a la base de conocimientos...");
 
-      result = result[Math.floor(Math.random()*length)];
+    console.log("Buscando información...");
 
-      if(typeof result === 'undefined'){
-        console.log("no encontré nada!");
-        db.close();
-        process.exit(1);
-      }
+    //busco los últimos, ordenado por fecha descendente y me quedo con los últimos 3
+    db.collection('ultimos').find({}).sort({fecha: -1}).limit(3).toArray((err, ultimos) => {
+      var ultIds = ultimos.map((x) => x.idLugar);
+      var ultimoTipoDeComida = typeof ultimos[0] == 'undefined' || ultimos[0].tipoDeComida == 'undefined'? [] : ultimos[0].tipoDeComida;
 
-      db.collection('ultimos').insert({idLugar: result._id, nombre: result.nombre, fecha: new Date(), tipoDeComida: result.tipoDeComida}, (err, records) => {
+      //busco todos los lugares disponibles sin contar los últimos seleccionados
+      db.collection('lugares').find({_id: {$nin: ultIds}, tipoDeComida: {$nin: ultimoTipoDeComida} , esLujo: false}).toArray((err, opcionesDeComida) => {
         if (err) console.log(err);
+        var result = opcionesDeComida;
 
-        db.collection('destinatarios').find().toArray((err, destinatarios) =>{
+        console.log("Aplicando algoritmo de desición...");
+
+        var length = result.length;
+
+        result = result[Math.floor(Math.random()*length)];
+
+        if(typeof result === 'undefined'){
+          console.log("no encontré nada!");
+          db.close();
+          process.exit(1);
+        }
+
+        db.collection('ultimos').insert({idLugar: result._id, nombre: result.nombre, fecha: new Date(), tipoDeComida: result.tipoDeComida}, (err, records) => {
           if (err) console.log(err);
 
-          mailer.sendMail(result, {name: "Claudio Yuri", email: process.env.EMAILSENDER}, destinatarios.map((x) => x.email));
+          db.collection('destinatarios').find().toArray((err, destinatarios) =>{
+            if (err) console.log(err);
 
-          //closedb connection
-          db.close();
+            mailer.sendMail(result, {name: "Claudio Yuri", email: process.env.EMAILSENDER}, destinatarios.map((x) => x.email));
+
+            //closedb connection
+            db.close();
+          });
         });
       });
     });
   });
-});
+}
